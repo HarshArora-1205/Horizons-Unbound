e14500a627e75486bbd4f571366493e1c808c502
Harsh Arora <iamharsharora1205@gmail.com>
2023-08-17 18:21:03 +0530

    Modified Card and Tech Section

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -60,10 +60,10 @@ const Trending = ({trendingPosts}: Props) => {
 
         {/* Grid */}
         <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
-            <TrendingCard className="col-span-2 row-span-2 bg-wh-500" post={trendingPosts[0]} />
-            <TrendingCard className="col-span-2 row-span-1 bg-wh-500" post={trendingPosts[1]} />
-            <TrendingCard className="col-span-1 row-span-1 bg-wh-500" post={trendingPosts[2]} />
-            <TrendingCard className="col-span-1 row-span-1 bg-wh-500" post={trendingPosts[3]} />
+            <TrendingCard className="col-span-2 row-span-2" post={trendingPosts[0]} />
+            <TrendingCard className="col-span-2 row-span-1" post={trendingPosts[1]} />
+            <TrendingCard className="col-span-1 row-span-1" post={trendingPosts[2]} />
+            <TrendingCard className="col-span-1 row-span-1" post={trendingPosts[3]} />
         </div>
 
         <p className="text-sm">
