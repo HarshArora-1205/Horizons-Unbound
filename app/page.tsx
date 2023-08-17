fbfcd335cb08227ffd34971227f00bbe3d3b52e1
Harsh Arora <iamharsharora1205@gmail.com>
2023-08-17 16:06:46 +0530

    Filtered Posts Data

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,30 @@ const getPosts = async () => {
 export default async function Home() {
   const posts = await getPosts();
 
+  const formatPosts = () => {
+    const trendingPosts: Array<Post> = [];
+    const techPosts: Array<Post> = [];
+    const travelPosts: Array<Post> = [];
+    const otherPosts: Array<Post> = [];
+
+    posts.forEach((post: Post, i: number) => {
+      if (i < 4) {
+        trendingPosts.push(post);
+      }
+      if (post?.category === "Tech") {
+        techPosts.push(post);
+      } else if (post?.category === "Travel") {
+        travelPosts.push(post);
+      } else if (post?.category === "Interior Design") {
+        otherPosts.push(post);
+      }
+    });
+
+    return [trendingPosts, techPosts, travelPosts, otherPosts];
+  }
+
+  const [trendingPosts, techPosts, travelPosts, otherPosts] = formatPosts();
+  
   return (
     <main className="leading-7 px-10">
       <Trending />
