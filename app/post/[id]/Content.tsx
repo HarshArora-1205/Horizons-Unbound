a33fd70de7097fbfe854f34f57eeb1c44cf1fb9a
Harsh Arora <iamharsharora1205@gmail.com>
2023-08-17 19:18:49 +0530

    Added Basic form header

--- app/post/[id]/Content.tsx
+++ app/post/[id]/Content.tsx
@@ -17,6 +17,10 @@ const Content = ({post}: Props) => {
   const [content, setContent] = useState<string>(post.content);
   const [contentError, setContentError] = useState<string>("");
 
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div className="prose w-full max-w-full mb-10">
       {/* BREADCRUMBS */}
@@ -41,6 +45,29 @@ const Content = ({post}: Props) => {
           )}
         </div>
       </div>
+
+      <form onSubmit={handleSubmit}>
+        {/* HEADER */}
+        <>
+          {isEditable ? (
+            <div>
+              <textarea
+                className="border-2 rounded-md bg-wh-50 p-3 w-full"
+                placeholder="Title"
+                onChange={(e) => {console.log("change title: ", e.target.value)}}
+                value={title}
+              />
+            </div>
+          ) : (
+            <h3 className="font-bold text-3xl mt-3">{title}</h3>
+          )}
+          <div className="flex gap-3">
+            <h5 className="font-semibold text-xs">By {post.author}</h5>
+            <h6 className="text-wh-300 text-xs">{post.createdAt}</h6>
+          </div>
+        </>
+
+      </form>
     </div>
   );
 }
