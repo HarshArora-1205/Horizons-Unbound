a8043be894fa5742ef3be1dd2e0415f037927886
Harsh Arora <iamharsharora1205@gmail.com>
2023-08-17 18:22:46 +0530

    Modified Travel Section color

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -22,24 +22,24 @@ const Travel = ({ travelPosts }: Props) => {
 
       <div className="sm:flex justify-between gap-8">
         <Card 
-          className="basis-1/3 mt-5 sm:mt-0 bg-wh-500" 
+          className="basis-1/3 mt-5 sm:mt-0" 
           post={travelPosts[0]}
           imageHeight='h-80'
         />
         <Card 
-          className="basis-1/3 mt-5 sm:mt-0 bg-wh-500" 
+          className="basis-1/3 mt-5 sm:mt-0" 
           post={travelPosts[1]}
           imageHeight='h-80'
         />
         <Card 
-          className="basis-1/3 mt-5 sm:mt-0 bg-wh-500"
+          className="basis-1/3 mt-5 sm:mt-0"
           post={travelPosts[2]} 
           imageHeight='h-80'
         />
       </div>
 
       <Card 
-        className="sm:flex justify-between items-center gap-3 mb-7 mt-5 bg-wh-500" 
+        className="sm:flex justify-between items-center gap-3 mb-7 mt-5" 
         post={travelPosts[3]}
         imageHeight='h-80'
       />
