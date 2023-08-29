b006d9855a7f647b152a3ff46a4a967f99377ac4
Harsh Arora <iamharsharora1205@gmail.com>
2023-08-29 15:10:39 +0530

    Added Comments/ OpenAI Backend

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,14 +1,20 @@
+// Imports
 import { NextResponse } from "next/server";
 import { OpenAI } from "openai";
 
+// Configure OpenAI
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// Define POST Request
 export async function POST(request: Request) {
   try {
+
+    // Parse data from request and fetch title & role
     const { title, role } = await request.json();
 
+    // Get auto generated AI Content
     const completion = await openai.chat.completions.create({
       messages: [
         { role: "user", content: `Create 3 line blog post with html tags based on this title: ${title}` },
@@ -17,13 +23,16 @@ export async function POST(request: Request) {
       model: "gpt-3.5-turbo",
     });
 
+    // Send the generated content
     return NextResponse.json(
       {
         content: completion.choices[0]?.message?.content,
       },
       { status: 200 }
     );
-  } catch (error) {
+  } 
+  // Catch Errors
+  catch (error) {
     console.error("request error", error);
     NextResponse.json({ error: "error updating post" }, { status: 500 });
   }
